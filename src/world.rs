d6b942eb3aa8958381ce97fcf9c9d5db9f697e34
christopherhjung <christopher.h.jung@web.de>
2023-02-01 23:36:04 +0100

    refactoring

--- src/world.rs
+++ src/world.rs
@@ -75,39 +75,6 @@ impl WorldImpl {
         def_ptr
     }
 
-    /*
-    pub fn construct(&mut self, def: DefLink) -> DefLink{
-        if def.kind == DefKind::Pending && DepCheck::valid(def){
-            CyclicSigner::sign( self, def)
-        }else{
-            def
-        }
-    }*/
-/*
-    pub fn sign(&mut self, link: DefLink, sign: &Signature) -> DefLink {
-        if let Some(mut def) = self.pending.remove(&link){
-            match self.sea.entry(*sign) {
-                Occupied(entry) =>
-                    DefLink::from(entry.get()),
-                Vacant(entry) => {
-                    def.kind = DefKind::Constructed(*sign);
-                    DefLink::from(entry.insert(def))
-                },
-            }
-        }else{
-            panic!()
-        }
-    }*/
-
-    /*
-    fn set_op(def_link: DefLink, val: DefLink, idx: usize){
-        def_link.ops.set(idx, val)
-    }
-
-    pub fn set_body(&mut self, lam: DefLink, body: DefLink){
-        Self::set_op(lam,body, 2);
-    }*/
-
     pub fn new_boxed() -> Box<WorldImpl>{
         let mut world = Box::new(WorldImpl {
             sign_size:  32,
@@ -134,85 +101,6 @@ impl World {
     fn as_mut(&self) -> *mut WorldImpl {
         self as *const _ as *mut WorldImpl
     }
-/*
-    pub fn new_def(&self, link : DefLink) -> Def{
-        Def{
-            world: self.impl_.clone(),
-            link
-        }
-    }
-
-    pub fn axiom( &mut self, ax : Axiom ) -> Def {
-        let link = self.impl_.axiom(ax);
-        self.new_def(link)
-    }*/
-/*
-    pub fn lit_int( &mut self, val : i32 ) -> Def {
-        let link = self.impl_.get().lit_int(val);
-        self.new_def(link)
-    }
-
-    pub fn ty_int( &mut self, width : i32 ) -> Def {
-        let link = self.impl_.get().ty_int(width);
-        self.new_def(link)
-    }
-
-    pub fn bot( &mut self ) -> Def {
-        self.axiom(Axiom::Bot)
-    }*/
-/*
-    pub fn tuple(&mut self, mut elems: Vec<Def> ) -> Def {
-        elems.insert(0, self.axiom(Axiom::Tuple));
-        self.new_def(elems)
-    }*/
-/*
-    pub fn pack(&mut self, shape: &Def, body: &Def) -> Def {
-        let link = self.impl_.get().pack(shape.link, body.link);
-        self.new_def(link)
-    }
-
-    pub fn extract(&mut self, tup: &Def, index: &Def) -> Def {
-        let link = self.impl_.get().extract(tup.link, index.link);
-        self.new_def(link)
-    }
-
-    pub fn app(&mut self, callee: &Def, arg: &Def) -> Def {
-        let link = self.impl_.get().app(callee.link, arg.link);
-        self.new_def(link)
-    }
-
-    pub fn pi(&mut self, domain: &Def, co_domain : &Def) -> Def {
-        let link = self.impl_.get().pi(domain.link, co_domain.link);
-        self.new_def(link)
-    }
-
-    pub fn lam(&mut self, ty : &Def) -> Def {
-        let link = self.impl_.get().lam(ty.link);
-        self.new_def(link)
-    }
-
-    pub fn var(&mut self, lam: &Def) -> Def {
-        let link = self.impl_.get().var(lam.link);
-        self.new_def(link)
-    }
-
-    pub fn set_body(&mut self, lam: &Def, body: &Def){
-        lam.set_op(2, body);
-    }*/
-/*
-    pub fn construct<const COUNT: usize>(&self, defs: [&Def; COUNT]) -> [Def; COUNT]{
-        let mut array: MaybeUninit<[Def; COUNT]> = MaybeUninit::uninit();
-        let mut ptr = array.as_mut_ptr() as *mut Def;
-
-        unsafe {
-            for def in defs{
-                ptr::write(ptr, self.new_def(def.link));
-                ptr = ptr.add(1);
-            }
-
-            array.assume_init()
-        }
-    }*/
 
     pub fn new() -> World{
         let world_impl = WorldImpl::new_boxed();
@@ -221,9 +109,7 @@ impl World {
             impl_: Rc::new(MutBox::from(world_impl))
         }
     }
-}
 
-impl World{
     pub fn builder(&self) -> Builder{
         Builder{
             world: self.impl_.clone(),
